43ba7b5152b9bd2cc4035d87da677a0f7c8502d7
riccardo <15861569+N0W3N@users.noreply.github.com>
2020-03-27 15:19:40 +0100

    Add page number argument -> it's now possible to scrape a desired amount of number of pages -> old code returned 5 pages by default
    Remove/reformat unneeded code
    -> removed both print statements for bs4, code now only returns the scraped urls
    -> renamed url variables for better visibility
    -> included f-strings for better visibility and usage
    -> including final return print statement with the amount of scraped links
    -> removed doubled variable for storing urls and moved it to regex function
    
    Fix listing indentation
    -> List of urls will be now returned when the whole scraping process is done
    -> Moved code out of the for loop
    -> Code has previously returned the list each time of the iteration, resulting in unnecessary terminal output

--- scanner.py
+++ scanner.py
@@ -4,36 +4,34 @@ import re
 import sys
 
 
-def scraper(keyword):
-    print('starting scraper')
+def scraper(keyword, number):
     listing = []
-    for i in range(0,4):
-        page = i*10
+    for i in range(0, int(number), 1):
+        PAGE = i*10
         URL = 'https://www.google.de/search'
-        PARAMETERS = {'q': str(keyword), 'start': int(page)}
+        PARAMETERS = {'q': str(keyword), 'start': int(PAGE)}
         #HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.2 (KHTML, like Gecko) Chrome/22.0.1216.0 Safari/537.2'}
         r = requests.get(URL, params=PARAMETERS) #headers=HEADERS)
         soup = BeautifulSoup(r.text, 'html.parser')
-        url_tag = soup.findAll('div')
-        for info in url_tag:
-            test1 = info
-            print(test1)
-            test = re.search(r"url\?q=(.+?)\&", str(test1))
-            print(test)
+        parse_url = soup.findAll('div')
+        for junk_url in parse_url:
+            test = re.search(r"url\?q=(.+?)\&", str(junk_url))
             if test is not None:
                 url = test.group(1)
                 listing.append(url)
             else:
                 pass
 
-        list = (set(listing))
-        print(*list, sep = '\n')
+    url_list = (set(listing))
+    print(*url_list, sep='\n')
+    print(f'Found {len(url_list)} results.')
 
 
 def main():
     keyword = sys.argv[1]
-    print('got keyword')
-    scraper(keyword=keyword)
+    number = sys.argv[2]
+    print(f'Starting Docking Scanner\n Looking for keyword {keyword} and returning {number} page/s of results.')
+    scraper(keyword=keyword, number=number)
 
 
 main()
